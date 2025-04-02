c016281934282723a7dd149cf205c70326e9940c
SouravBhattacharyya <sourav18sb@gmail.com>
2025-04-02 13:40:41 +0530

    Update MobileAccordition.tsx

--- src/components/Header/MobileAccordition.tsx
+++ src/components/Header/MobileAccordition.tsx
@@ -8,6 +8,13 @@ import {
 import { Separator } from "../ui/separator";
 
 const MobileAccordition = () => {
+  const demo = [
+    { id: 1, name: "technology" },
+    { id: 2, name: "review" },
+    { id: 3, name: "sports" },
+    { id: 4, name: "news" },
+  ];
+
   return (
     <>
       <div className="">
@@ -20,20 +27,23 @@ const MobileAccordition = () => {
               Category
             </AccordionTrigger>
 
-            <AccordionContent className="grid gap-3 ps-3 pt-3">
-              <Link href={"/"}>Technology</Link>
-
-              <Separator />
-
-              <Link href={"/"}>Review</Link>
-
-              <Separator />
-
-              <Link href={"/"}>Sports</Link>
-
-              <Separator />
-
-              <Link href={"/"}>News</Link>
+            <AccordionContent>
+              {demo.map((item) => {
+                return (
+                  <div
+                    className="grid gap-3 ps-3 pt-3"
+                    key={item.id}
+                  >
+                    <Link
+                      className="capitalize"
+                      href={"/"}
+                    >
+                      {item.name}
+                    </Link>
+                    <Separator />
+                  </div>
+                );
+              })}
             </AccordionContent>
           </AccordionItem>
         </Accordion>
